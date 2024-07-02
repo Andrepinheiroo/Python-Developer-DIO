426f51ad8f7b2fb26688154f8353390b604f1f19
Andrepinheiroo <andrepinheiro.code@gmail.com>
2024-07-01 23:41:57 -0300

    Update Variaves_e_Constantes.py

--- Codigos Aulas/Explorando a lingugem/Variaves_e_Constantes.py
+++ Codigos Aulas/Explorando a lingugem/Variaves_e_Constantes.py
@@ -1,2 +1,15 @@
 age, name = (25, "André Pinheiro")
 print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
+
+age = 24
+name = "sabrina"
+print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
+
+print(age,name)
+
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["PE", "RJ", "RN", "SP"]
+
+print(BRAZILIAN_STATES)
